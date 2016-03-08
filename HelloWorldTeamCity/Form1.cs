251bbf8e5554bab9de37a0e5c7636b4b7baa8f47
WarrenPickles <warrenspickles@gmail.com>
2016-03-08 11:04:31 +0000

    Initial commit from VS

---
+++ HelloWorldTeamCity/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldTeamCity {
+    public partial class HelloWorldForm : Form {
+
+        public HelloWorldForm( ) {
+            InitializeComponent( );
+        }
+
+        private void buttonOK_Click( object sender, EventArgs e ) {
+            this.DialogResult = DialogResult.OK;
+            this.Close( );
+        }
+
+    }
+}
---
+++ HellowWorldUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorldTeamCity;
+using System.Windows.Forms;
+
+namespace HellowWorldUnitTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1( ) {
+            HelloWorldForm helloWorldForm = new HelloWorldForm( );
+            Assert.AreEqual( "Hello World!", helloWorldForm.label1.Text );
+        }
+    }
+}
